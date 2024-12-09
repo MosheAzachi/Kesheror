2881461356e022fefdbbb9d3204950f724284cf9
Moshe <azachi.moshiko@gmail.com>
2024-12-09 20:12:10 +0200

    change cors setting in backend

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ const contactRouter = require("./routes/contactRoute");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
