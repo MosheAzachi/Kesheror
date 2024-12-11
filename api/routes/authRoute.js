c4c58c4da19368c87f9642dfeb7b785bb356fde6
Moshe <azachi.moshiko@gmail.com>
2024-12-11 13:34:22 +0200

    set authMiddleware on some routes

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 
 const authController = require("./../controllers/authController");
+const authMiddleware = require("./../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", authController.getUsers);
+router.get("/", authMiddleware.protect, authMiddleware.restrict, authController.getUsers);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/me", authController.getCurrentUser);
 router.get("/logout", authController.logout);
-router.patch("/:id/set-admin", authController.setAdmin);
-router.delete("/:id", authController.deleteUser);
+router.patch("/:id/set-admin", authMiddleware.protect, authMiddleware.restrict, authController.setAdmin);
+router.delete("/:id", authMiddleware.protect, authMiddleware.restrict, authController.deleteUser);
 
 module.exports = router;
--- api/routes/contactRoute.js
+++ api/routes/contactRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const contactController = require("./../controllers/contactController");
+const authMiddleware = require("./../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", contactController.sendContactRequest);
-router.get("/", contactController.getContacts);
-router.delete("/:id", contactController.deleteContact);
+router.get("/", authMiddleware.protect, authMiddleware.restrict, contactController.getContacts);
+router.delete("/:id", authMiddleware.protect, authMiddleware.restrict, contactController.deleteContact);
 
 module.exports = router;
